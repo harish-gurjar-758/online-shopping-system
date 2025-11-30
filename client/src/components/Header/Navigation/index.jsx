58928f6e5c136484e72c1ff8181d2e53b0df5800
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-11-30 18:21:34 +0530

    .

--- client/src/components/Header/Navigation/index.jsx
+++ client/src/components/Header/Navigation/index.jsx
@@ -1,74 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import { HiMenuAlt1 } from "react-icons/hi";
 import { TfiAngleDown } from "react-icons/tfi";
 import { Link } from 'react-router-dom';
 import { GoRocket } from "react-icons/go";
+import CategoryPanel from './CategoryPanel';
 
 export default function Navigation() {
+  const [isOpenCategoryPanel, setIsOpenCategory] = useState(false)
+  const openCategoryPanel = () => {
+    setIsOpenCategory(true);
+  }
   return (
-    <nav className='py-2'>
-      <div className="container flex items-center justify-end gap-5">
-        <div className="col_1 w-[20%]">
-          <Button className='!text-black gap-2 w-full'>
-            <HiMenuAlt1 className='!text-[18px]' />
-            Shop By Categories
-            <TfiAngleDown className='text-[14px] ml-auto font-bold' />
-          </Button>
-        </div>
+    <>
+      {/* --Start-- */}
+      <nav className='py-2'>
+        <div className="container flex items-center justify-end gap-5">
+          <div className="col_1 w-[20%]">
+            <Button
+              className='!text-black gap-2 w-full'
+              onClick={openCategoryPanel}
+            >
+              <HiMenuAlt1 className='!text-[18px]' />
+              Shop By Categories
+              <TfiAngleDown className='text-[14px] ml-auto font-bold' />
+            </Button>
+          </div>
 
-        <div className="col_2 w-[62%]">
-          <ul className="flex items-center justify-evenly gap-2">
-            <li className='list-none'>
-              <Link className='link transition text-[15px] font-[500]'>
-                <Button className='link transition'>Home</Button>
-              </Link>
-            </li>
-            <li className='list-none'>
-              <Link className='link transition text-[15px] font-[500]'>
-                <Button className='link transition'>Fashion</Button>
-              </Link>
-            </li>
-            <li className='list-none'>
-              <Link className='link transition text-[15px] font-[500]'>
-                <Button className='link transition'>Bags</Button>
-              </Link>
-            </li>
-            <li className='list-none'>
-              <Link className='link transition text-[15px] font-[500]'>
-                <Button className='link transition'>Footwear</Button>
-              </Link>
-            </li>
-            <li className='list-none'>
-              <Link className='link transition text-[15px] font-[500]'>
-                <Button className='link transition'>Groceries</Button>
-              </Link>
-            </li>
-            <li className='list-none'>
-              <Link className='link transition text-[15px] font-[500]'>
-                <Button className='link transition'>Beauty</Button>
-              </Link>
-            </li>
-            <li className='list-none'>
-              <Link className='link transition text-[15px] font-[500]'>
-                <Button className='link transition'>Wellness</Button>
-              </Link>
-            </li>
-            <li className='list-none'>
-              <Link className='link transition text-[15px] font-[500]'>
-                <Button className='link transition'>Jewellery</Button>
-              </Link>
-            </li>
-          </ul>
-        </div>
+          <div className="col_2 w-[62%]">
+            <ul className="flex items-center justify-evenly gap-1">
+              <li className='list-none'>
+                <Link className='link transition text-[15px] '>
+                  <Button className='link transition font-[500]'>Home</Button>
+                </Link>
+              </li>
+              <li className='list-none'>
+                <Link className='link transition text-[15px] '>
+                  <Button className='link transition font-[500]'>Fashion</Button>
+                </Link>
+              </li>
+              <li className='list-none'>
+                <Link className='link transition text-[15px]'>
+                  <Button className='link transition font-[500]'>Bags</Button>
+                </Link>
+              </li>
+              <li className='list-none'>
+                <Link className='link transition text-[15px]'>
+                  <Button className='link transition font-[500]'>Footwear</Button>
+                </Link>
+              </li>
+              <li className='list-none'>
+                <Link className='link transition text-[15px]'>
+                  <Button className='link transition font-[500]'>Groceries</Button>
+                </Link>
+              </li>
+              <li className='list-none'>
+                <Link className='link transition text-[15px]'>
+                  <Button className='link transition font-[500]'>Beauty</Button>
+                </Link>
+              </li>
+              <li className='list-none'>
+                <Link className='link transition text-[15px]'>
+                  <Button className='link transition font-[500]'>Wellness</Button>
+                </Link>
+              </li>
+              <li className='list-none'>
+                <Link className='link transition text-[15px]'>
+                  <Button className='link transition font-[500]'>Jewellery</Button>
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-        <div className="col_3 w-[18%] text-center justify-center">
-          <p className='text-[13px] font-[500] flex text-center justify-center gap-1 mb-0 mt-0'>
-            <GoRocket fontSize={18} />
-            Free International Delivery
-          </p>
+          <div className="col_3 w-[18%] text-center justify-center">
+            <p className='text-[13px] font-[500] flex text-center justify-center gap-1 mb-0 mt-0'>
+              <GoRocket fontSize={18} />
+              Free International Delivery
+            </p>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      {/* --End-- */}
+
+      {/* Category Panel component */}
+      {/* --Start-- */}
+      <CategoryPanel
+        // openCategoryPanel={openCategoryPanel}
+        isOpenCategoryPanel={isOpenCategoryPanel}
+        setIsOpenCategory={setIsOpenCategory}
+      />
+      {/* --End-- */}
+    </>
   );
 }
