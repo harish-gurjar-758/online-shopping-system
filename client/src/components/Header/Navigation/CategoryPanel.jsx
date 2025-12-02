c276216b88b815998eebf79bc864227919cdcb02
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-12-02 11:53:45 +0530

    update the category nav layout

--- client/src/components/Header/Navigation/CategoryPanel.jsx
+++ client/src/components/Header/Navigation/CategoryPanel.jsx
@@ -7,8 +7,74 @@ import CloseIcon from "@mui/icons-material/Close";
 import { FiPlusSquare } from "react-icons/fi";
 import { RiSubtractLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
+import logo from '../../../../public/logo.png'
+
+// CategoryData
+const categoryNavData = [
+    {
+        id: "01CT",
+        categoryName: "Fashion",
+        subCategories: [
+            { id: "01CTsubCatFs01", name: "Boys" },
+            { id: "01CTsubCatFs02", name: "Girls" },
+            { id: "01CTsubCatFs03", name: "Mens" },
+            { id: "01CTsubCatFs04", name: "Womens" },
+        ],
+    },
+
+    {
+        id: "02CT",
+        categoryName: "Electronics",
+        subCategories: [
+            { id: "01CTsubCatEl01", name: "Smart Watch" },
+            { id: "01CTsubCatEl02", name: "Laptops" },
+            { id: "01CTsubCatEl03", name: "Mobiles" },
+            { id: "01CTsubCatEl04", name: "Tablets" },
+        ],
+    },
+
+    {
+        id: "03CT",
+        categoryName: "Bags",
+        subCategories: [
+            { id: "01CTsubCatBg01", name: "Chaldrons Bag" },
+            { id: "01CTsubCatBg02", name: "Mens Bag" },
+            { id: "01CTsubCatBg03", name: "Womens Bag" },
+        ],
+    },
+
+    {
+        id: "04CT",
+        categoryName: "Footwear",
+        subCategories: [
+            { id: "01CTsubCatFt01", name: "Boys Footwear" },
+            { id: "01CTsubCatFt02", name: "Childrens Footwear" },
+            { id: "01CTsubCatFt03", name: "Girls Footwear" },
+            { id: "01CTsubCatFt04", name: "Mens Footwear" },
+            { id: "01CTsubCatFt05", name: "Womens Footwear" },
+        ],
+    },
+
+    { id: "05CT", categoryName: "Groceries" },
+    { id: "06CT", categoryName: "Beauty" },
+    { id: "07CT", categoryName: "Wellness" },
+    { id: "08CT", categoryName: "Jewellery" },
+];
 
 export default function CategoryPanel(props) {
+
+    // Dynamic open/close states
+    const [openCategory, setOpenCategory] = useState({});
+    const [openSubCategory, setOpenSubCategory] = useState({});
+
+    const toggleCat = (catId) => {
+        setOpenCategory((prev) => ({ ...prev, [catId]: !prev[catId] }));
+    };
+
+    const toggleSubCat = (subId) => {
+        setOpenSubCategory((prev) => ({ ...prev, [subId]: !prev[subId] }));
+    };
+
     const [openMain, setOpenMain] = useState({
         fashion: false,
         outerwear: false,
@@ -34,7 +100,8 @@ export default function CategoryPanel(props) {
 
     const DrawerList = (
         <Box sx={{ width: 280 }} role="presentation" className='categoryPanel'>
-            <div className="flex items-center justify-between p-3">
+            <img src={logo} alt="" />
+            <div className="flex items-center justify-between flex-wrap p-3">
                 <h3 className="text-[16px] font-[500]">Shop By Category</h3>
                 <CloseIcon className="cursor-pointer" onClick={toggleDrawer(false)} />
             </div>
@@ -43,148 +110,56 @@ export default function CategoryPanel(props) {
 
             <div className="scroll">
                 <ul className="w-full">
+                    {categoryNavData.map((cat) => (
+                        <li
+                            key={cat.id}
+                            className="list-none w-full flex items-center justify-between flex-wrap relative"
+                        >
+                            <Link to='' className="link w-full">
+                                <Button
+                                    disableRipple
+                                    className="w-full !text-left !justify-start !px-3 link"
+                                    // onClick={() => cat.subCategories && toggleCat(cat.id)}
+                                >
+                                    {cat.categoryName}
+                                </Button>
 
-                    {/* -------------------- FASHION -------------------- */}
-                    <li className="list-none w-full flex items-center justify-between relative">
-                        <Link to='' className="link w-full">
-                            <Button
-                                disableRipple
-                                className="w-full !text-left !justify-start !px-3 link"
-                            // onClick={() => toggleMain("fashion")}
-                            >
-                                Fashion
-                            </Button>
-                        </Link>
-
-                        {openMain.fashion ? (
-                            <RiSubtractLine
-                                className="absolute right-3 top-3 cursor-pointer"
-                                onClick={() => toggleMain("fashion")}
-                            />
-                        ) : (
-                            <FiPlusSquare
-                                className="absolute right-3 top-3 cursor-pointer"
-                                onClick={() => toggleMain("fashion")}
-                            />
-                        )}
-
-                        {openMain.fashion && (
-                            <ul className="submenu absolute top-[100%] left-0 w-full pl-3">
-
-                                {/* ---------- Apparel ---------- */}
-                                <li className="list-none flex items-center justify-between relative">
-                                    <Link to='' className="link w-full">
-                                        <Button
-                                            disableRipple
-                                            className="w-full !text-left !justify-start !px-3 link"
-                                            onClick={() => toggleSub("apparel")}
-                                        >
-                                            Apparel
-                                        </Button>
-                                    </Link>
-
-                                    {openSub.apparel ? (
+                                {cat.subCategories && (
+                                    openCategory[cat.id] ? (
                                         <RiSubtractLine
                                             className="absolute right-3 top-3 cursor-pointer"
-                                            onClick={() => toggleSub("apparel")}
+                                            onClick={() => toggleCat(cat.id)}
                                         />
                                     ) : (
                                         <FiPlusSquare
                                             className="absolute right-3 top-3 cursor-pointer"
-                                            onClick={() => toggleSub("apparel")}
+                                            onClick={() => toggleCat(cat.id)}
                                         />
-                                    )}
-
-                                    {openSub.apparel && (
-                                        <ul className="ml-3 bg-white">
-                                            <li><Link to="/dsfd" className="block py-2 px-3">Smart Tablet</Link></li>
-                                            <li><Link to="/dsfd" className="block py-2 px-3">Crepe T-Shirt</Link></li>
-                                            <li><Link to="/dsfd" className="block py-2 px-3">Leather Watch</Link></li>
-                                            <li><Link to="/dsfd" className="block py-2 px-3">Rolling Diamond</Link></li>
-                                        </ul>
-                                    )}
-                                </li>
-
-                                {/* ---------- Outerwear ---------- */}
-                                <li className="relative">
-                                    <Button
-                                        disableRipple
-                                        className="w-full !text-left !justify-start !px-3 link"
-                                        onClick={() => toggleSub("outerwear")}
-                                    >
-                                        Outerwear
-                                    </Button>
-
-                                    {openSub.outerwear ? (
-                                        <RiSubtractLine
-                                            className="absolute right-3 top-3 cursor-pointer"
-                                            onClick={() => toggleSub("outerwear")}
-                                        />
-                                    ) : (
-                                        <FiPlusSquare
-                                            className="absolute right-3 top-3 cursor-pointer"
-                                            onClick={() => toggleSub("outerwear")}
-                                        />
-                                    )}
-
-                                    {openSub.outerwear && (
-                                        <ul className="ml-3 bg-white">
-                                            <li><Link to="/dsfd" className="block py-2 px-3">Jackets</Link></li>
-                                            <li><Link to="/dsfd" className="block py-2 px-3">Hoodies</Link></li>
-                                            <li><Link to="/dsfd" className="block py-2 px-3">Coats</Link></li>
-                                        </ul>
-                                    )}
-                                </li>
-
-                                {/* ---------- Footwear ---------- */}
-                                <li className="relative">
-                                    <Button
-                                        disableRipple
-                                        className="w-full !text-left !justify-start !px-3 link"
-                                        onClick={() => toggleSub("footwear")}
-                                    >
-                                        Footwear
-                                    </Button>
-
-                                    {openSub.footwear ? (
-                                        <RiSubtractLine
-                                            className="absolute right-3 top-3 cursor-pointer"
-                                            onClick={() => toggleSub("footwear")}
-                                        />
-                                    ) : (
-                                        <FiPlusSquare
-                                            className="absolute right-3 top-3 cursor-pointer"
-                                            onClick={() => toggleSub("footwear")}
-                                        />
-                                    )}
-
-                                    {openSub.footwear && (
-                                        <ul className="ml-3 bg-white">
-                                            <li><Link to="/dsfd" className="w-full link block py-2 px-3">Sneakers</Link></li>
-                                            <li><Link to="/dsfd" className="w-full block py-2 px-3 link">Sandals</Link></li>
-                                            <li><Link to="/dsfd" className="w-full block py-2 px-3 link">Boots</Link></li>
-                                        </ul>
-                                    )}
-                                </li>
-
-                            </ul>
-                        )}
-                    </li>
-
-                    {/* -------------------- Simple Categories -------------------- */}
-                    <li className="list-none flex items-center justify-between">
-                        <Link to='/jewellery' className="link w-full">
-                            <Button
-                                className="w-full !text-left !justify-start !px-3 link"
-                            >Jewellery</Button>
-                        </Link>
-                    </li>
-                    <li className="list-none flex items-center justify-between relative">
-                        <Link to='' className="link w-full">
-                            <Button className="w-full !text-left !justify-start !px-3 link">Watches</Button>
-                        </Link>
-                    </li>
-
+                                    )
+                                )}
+                                {openCategory[cat.id] && cat.subCategories && (
+                                    <ul className="submenu relative w-full pl-3">
+                                        {cat.subCategories.map((sub) => (
+                                            <li
+                                                key={sub.id}
+                                                className="list-none flex items-center justify-between relative"
+                                            >
+                                                <Link to='' className="link w-full">
+                                                    <Button
+                                                        disableRipple
+                                                        className="w-full !text-left !justify-start !px-3 link"
+                                                        onClick={() => toggleSubCat(sub.id)}
+                                                    >
+                                                        {sub.name}
+                                                    </Button>
+                                                </Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                )}
+                            </Link>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </Box>
