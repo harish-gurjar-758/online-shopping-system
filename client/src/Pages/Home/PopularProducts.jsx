9354ec68f7d0beafc81218750cd66943af6d62e9
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-12-06 15:52:53 +0530

    .

--- client/src/Pages/Home/PopularProducts.jsx
+++ client/src/Pages/Home/PopularProducts.jsx
@@ -5,6 +5,7 @@ import StarIcon from '@mui/icons-material/Star';
 import StarHalfIcon from '@mui/icons-material/StarHalf';
 import StarOutlineIcon from '@mui/icons-material/StarOutline';
 import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
+import { Link } from 'react-router-dom'
 
 export default function PopularProducts() {
     return (
@@ -19,7 +20,10 @@ export default function PopularProducts() {
                 </div>
             </div>
             <div className='w-full flex items-center justify-center gap-2'>
-                <div className="card border border-l-rose-100">
+                <Link 
+                to={'/product-details'}
+                className="card border border-l-rose-100"
+                >
                     <div className='img w-full h-[200px] overflow-hidden'>
                         <img src="https://th.bing.com/th/id/R.4aea12329f76bd36486da9a7082833b7?rik=PdNoh71YSdvefw&riu=http%3a%2f%2fblog.southindiajewels.com%2fwp-content%2fuploads%2f2018%2f02%2fgold-jewellery-sets-for-marriage-19.jpg&ehk=q9Tj80NdD0xrUQ1pHn9Q4cEcXVsGprUyKWwiyyOIDnw%3d&risl=&pid=ImgRaw&r=0" alt="" />
                     </div>
@@ -43,9 +47,13 @@ export default function PopularProducts() {
                                 1000.00
                             </p>
                         </div>
-                        <Button className='cart-btn'> <LocalGroceryStoreIcon /> Add to cart</Button>
+                        <Button className='cart-btn'>
+                            <Link to={'/add to cart'}>
+                                <LocalGroceryStoreIcon /> Add to cart
+                            </Link>
+                        </Button>
                     </div>
-                </div>
+                </Link>
                 <div className="card border border-l-rose-100">
                     <div className='img w-full h-[200px] overflow-hidden'>
                         <img src="https://th.bing.com/th/id/R.4aea12329f76bd36486da9a7082833b7?rik=PdNoh71YSdvefw&riu=http%3a%2f%2fblog.southindiajewels.com%2fwp-content%2fuploads%2f2018%2f02%2fgold-jewellery-sets-for-marriage-19.jpg&ehk=q9Tj80NdD0xrUQ1pHn9Q4cEcXVsGprUyKWwiyyOIDnw%3d&risl=&pid=ImgRaw&r=0" alt="" />
