79aace7b5133d47a1ff2617bdb3331e862205c45
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-12-11 22:43:57 +0530

    .

--- client/src/components/Home/BigSavingDaysSale.jsx
+++ client/src/components/Home/BigSavingDaysSale.jsx
@@ -26,10 +26,10 @@ export default function BigSavingDaysSale() {
     ];
 
     return (
-        <div className='w-full'>
+        <div className='w-full py-5 mb-5'>
             <div className="container flex items-center justify-center gap-10">
 
-                <div className='w-[60%] h-[533px] '>
+                <div className='w-[70%] h-[533px] '>
                     <Swiper
                         modules={[Navigation, Autoplay]}
                         navigation
@@ -57,33 +57,29 @@ export default function BigSavingDaysSale() {
                     </Swiper>
                 </div>
 
-                <div className='container w-[40%] flex flex-col items-center justify-center gap-4'>
-                    <div
-                        className='w-[425px] h-[285px] relative rounded-md'
-                    >
+                <div className='part2 scrollableBox w-full lg:w-[30%] flex items-center gap-5 justify-between flex-row lg:flex-col'>
+                    <div className='bannerBoxV2 box w-full overflow-hidden rounded-md group relative'>
                         <img
                             src="https://serviceapi.spicezgold.com/download/1760160666204_1737020916820_New_Project_52.jpg" alt=""
                             className='w-full transition-all duration-150 group-hover:scale-105'
                         />
-                        <div className='info absolute p-5 top-0 right-0 w-[70%] h-[100%] z-50 flex items-center justify-center flex-col gap-2 pl-16'>
-                            <h3 className='text-[25px] font-[500]'>Buy women with low price</h3>
-                            <p className='text-[20px]  text-[#ff5252] font-[500]'>₹999</p>
-                            <Link className='uppercase underline'>Shop Now</Link>
+                        <div className='info absolute p-5 top-0 right-0 w-[70%] h-[100%] z-50 flex items-center justify-center flex-col gap-2 '>
+                            <h2 className='text-[14px] md:text-[18px] font-[600]'>Buy women with low price</h2>
+                            <p className='text-[20px] text-primary font-[600] w-full'>₹999</p>
+                            <Link className='text-[16px] font-[600] link underline'>Shop Now</Link>
 
                         </div>
                     </div>
 
-                    <div
-                        className='w-[425px] h-[285px] relative rounded-md'
-                    >
+                    <div className='bannerBoxV2 box w-full overflow-hidden rounded-md group relative'>
                         <img
                             src="https://serviceapi.spicezgold.com/download/1741664665391_1741497254110_New_Project_50.jpg" alt=""
                             className='w-full transition-all duration-150 group-hover:scale-105'
                         />
-                        <div className='info absolute p-5 top-0 right-0 w-[70%] h-[100%] z-50 flex items-center justify-center flex-col gap-2 pl-16'>
-                            <h3 className='text-[25px] font-[500]'>Buy Men's Footwear with low price</h3>
-                            <p className='text-[20px]  text-[#ff5252] font-[500]'>₹1500</p>
-                            <Link className='uppercase underline'>Shop Now</Link>
+                        <div className='info absolute p-5 top-0 right-0 w-[70%] h-[100%] z-50 flex items-center justify-center flex-col gap-2'>
+                            <h2 className='text-[14px] md:text-[18px] font-[600]'>Buy Men's Footwear with low price</h2>
+                            <p className='text-[20px] text-primary font-[600] w-full'>₹1500</p>
+                            <Link className='text-[16px] font-[600] link underline'>Shop Now</Link>
                         </div>
                     </div>
                 </div>
