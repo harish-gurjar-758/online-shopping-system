38e567de57b379633ec41532a325f453bfaf3d41
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-12-25 13:41:20 +0530

    set the routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Pages/Login";
 import SignUp from "./Pages/SignUp";
+import NavBar from "./Components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/Signup" element={<SignUp />} />
---
+++ admin/src/Components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WebLogo from '../../../public/logo.png';
+
+export default function NavBar() {
+    return (
+        <div>
+            <div>
+                <img src={WebLogo} alt="web logo" />
+            </div>
+        </div>
+    )
+}
