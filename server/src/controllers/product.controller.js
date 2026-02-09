8514e26d96ec714837379f68e2b911ddd70bca2a
harish-gurjar-758 <gurjarharish758@gmail.com>
2026-02-09 15:24:30 +0530

    reate get product by id api

--- server/src/controllers/product.controller.js
+++ server/src/controllers/product.controller.js
@@ -95,6 +95,27 @@ export const getProductCount = async (req, res) => {
   }
 };
 
-export const getProductById = async (req, res) => { }
+export const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found !"
+      });
+    }
+
+    res.json({
+      success: true,
+      product
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+}
 export const updateProduct = async (req, res) => { }
 export const deleteProduct = async (req, res) => { }
